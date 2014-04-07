30faa55721ec06afe4addda94cfaa9d987027ad0
unknown <astring@astring-pc.aved.local>
2014-04-07 14:15:14 -0400

    docnum: - Updated search behavior to include search by date (using regex) - Added latest 10 modified doc list to home page - Began added handling of detail records
    Signed-off-by: unknown <astring@astring-pc.aved.local>

--- docnums.php
+++ docnums.php
@@ -59,7 +59,7 @@ class doctable {
 		foreach($tabledata as $item){
 			echo "
 				<tr>
-					<td align='center'>".$item['number']."</td>
+					<td align='center'><a href='".$_SERVER['PHP_SELF']."?doc=".$item['number']."'>".$item['number']."</td>
 					<td align='center'>".$item['rev']."</td>
 					<td align='left'>".$item['customer']."</td>
 					<td align='left'>".$item['description']."</td>
@@ -75,7 +75,7 @@ class doctable {
 }
 
 //Create shortcut menu on the top of the page for easy navigation
-echo "<p align='center'><b>| <a href='docnums.php'>Home</a> | ";
+echo "<p align='center'>Shortcuts: <b>| <a href='docnums.php'>All</a> | ";
 
 for ($cat = 0; $cat <= max(array_map("max", $docnumindex))-1; $cat++) {
 	echo "<a href='docnums.php?cat=".$docnumindex[$cat][0]."'>".$docnumindex[$cat][1]."</a> | ";
@@ -99,45 +99,34 @@ echo "</b></p>";
 
 <?php
 
-//Categories:
-// 1 = ECOs 				ECO1###
-// 2 = Electrical Parts 	AV-01-1####
-// 3 = Mechanical Parts 	AV-02-2####
-// 4 = Fixtrues 			AV-02-1####
-// 5 = Assemblies 			AV-03-1####
-// 6 = Documents 			AV-04-1####	
-// 7 = Software 			AV-05-1####
-
 //If we have NO post data
-if(empty($_POST)){
-	//echo "empty POST<br>";
+if(!isset($_POST['cat'])&&!isset($_GET['cat'])&&!isset($_GET['doc'])){
+	//Show the 10 most recently updated documents
+	$result = mysqli_query($mysqli,"SELECT * FROM docnums WHERE 1 ORDER BY datemodified DESC LIMIT 0,10;");
+		//Store the database results in an array
+		$searchresults = array();
+		while ($row = mysqli_fetch_array($result)){
+			$searchresults[] = $row;	//Array of doc arrays
+		}
+	echo "Ten most recently updated documents:<br><br>";
+	$table = new doctable;
+	$table->maketable($searchresults);
 }
-
-//Database tables: batfadb -> docnums -> fields:
-// id
-// number
-// rev
-// customer
-// description
-// workorder
-// author
-// datecreated (auto)
-// datemodified
-// project
 
 //If a menu category is clicked	
 if(isset($_GET['cat'])){
 	$cat = $_GET['cat'];
-	echo "<h2>".$docnumindex[$cat - 1][1]."</h2><br>"; //Show the category name under the hr
-	//Display a filtered list of documents
-	echo "Showing all <b>".$docnumindex[$cat - 1][1]."</b>. These all start with \"".$docnumindex[$cat - 1][2]."\"<br><br>";
+	
 	$result = mysqli_query($mysqli,"SELECT * FROM docnums WHERE number REGEXP '".$docnumindex[$cat - 1][2]."' ORDER BY datemodified DESC;");
 	//Store the database results in an array
 	$searchresults = array();
 	while ($row = mysqli_fetch_array($result)){
 		$searchresults[] = $row;	//Array of doc arrays
 	}
-	
+
+	echo "<h2>".$docnumindex[$cat - 1][1]."</h2><br>"; //Show the category name under the hr
+	//Display a filtered list of documents
+	echo "Showing all <b>".$docnumindex[$cat - 1][1]."</b>. These all start with \"".$docnumindex[$cat - 1][2]."\"<br><br>";
 	$table = new doctable;
 	$table->maketable($searchresults);
 }
@@ -146,26 +135,75 @@ if(isset($_GET['cat'])){
 //First, the general case where no category is selected:
 if(isset($_POST['searchtext'])&&!isset($_POST['cat'])){
 
-	echo "Showing search results for <b>".$_POST['searchtext']."</b>:<br><br>";
-	
-	$searchtext = "%".$_POST['searchtext']."%"; //Add a "%" before and after to the search term in order to grab anything before/after the text - otherwise it will only key as "starts with" instead of "contains".
 
-	$result = mysqli_query($mysqli,"SELECT * FROM docnums WHERE number LIKE '".$searchtext."' OR customer LIKE '".$searchtext."' OR description LIKE '".$searchtext."' OR workorder LIKE '".$searchtext."' OR author LIKE '".$searchtext."' ORDER BY datemodified DESC");
-	//Store the database results in an array
-	$searchresults = array();
-	while ($row = mysqli_fetch_array($result)){
-		$searchresults[] = $row;	//Array of doc arrays
+	//If user enters a date range, process it.
+	if (preg_match('/(\d+\/\d+\/\d+)\s*-\s*(\d+\/\d+\/\d+)/',$_POST['searchtext'],$searchdate)) {
+		;
+		//Format date inputs into proper dates
+		$searchdate1 = date('Y-m-d',strtotime($searchdate[1]));
+		$searchdate2 = date('Y-m-d',strtotime($searchdate[2]));
+
+		echo "Searching between <b>".$searchdate1."</b> and <b>".$searchdate2."</b>:<br><br>";
+
+		$result = mysqli_query($mysqli,"SELECT * FROM docnums WHERE datemodified BETWEEN '".$searchdate1."' AND '".$searchdate2."' ORDER BY datemodified DESC;");
+		//Store the database results in an array
+		$searchresults = array();
+		while ($row = mysqli_fetch_array($result)){
+			$searchresults[] = $row;	//Array of doc arrays
+		}
+	}
+
+	//Otherwise, the search must be for some text string.
+	else {
+		echo "Search results for everything containing <b>".$_POST['searchtext']."</b>:<br><br>";
+		$searchtext = "%".$_POST['searchtext']."%"; //Add a "%" before and after to the search term in order to grab anything before/after the text - otherwise it will only key as "starts with" instead of "contains".
+
+		$result = mysqli_query($mysqli,"SELECT * FROM docnums WHERE number LIKE '".$searchtext."' OR customer LIKE '".$searchtext."' OR description LIKE '".$searchtext."' OR workorder LIKE '".$searchtext."' OR author LIKE '".$searchtext."' ORDER BY datemodified DESC");
+		//Store the database results in an array
+		$searchresults = array();
+		while ($row = mysqli_fetch_array($result)){
+			$searchresults[] = $row;	//Array of doc arrays
+		}
 	}
 	
 	$table = new doctable;
 	$table->maketable($searchresults);
 
 }
-//Second, the case where a specific category is selected:
+
+//Second, the case where a specific category has been selected using a POST
 if(isset($_POST['searchtext'])&&isset($_POST['cat'])){
 	//If the post contains the category, use it.
-	echo "Search results for <b>".$_POST['searchtext']."</b>:<br>
-			[placeholder for search results for \"".$_POST['searchtext']."\" in category ".$_POST['cat']."]";
+	$cat = $_POST['cat'];
+	$searchtext = "%".$_POST['searchtext']."%"; //Add a "%" before and after to the search term in order to grab anything before/after the text - otherwise it will only key as "starts with" instead of "contains".
+	echo "<h2>".$docnumindex[$cat - 1][1]."</h2><br>"; //Show the category name under the hr
+	//Display a filtered list of documents
+	echo "Search results for <b>".$docnumindex[$cat - 1][1]."</b> containing <b>".$_POST['searchtext']."</b>:<br><br>";
+	$result = mysqli_query($mysqli,"SELECT * FROM docnums WHERE number REGEXP '".$docnumindex[$cat - 1][2]."' AND (number LIKE '".$searchtext."' OR customer LIKE '".$searchtext."' OR description LIKE '".$searchtext."' OR workorder LIKE '".$searchtext."' OR author LIKE '".$searchtext."') ORDER BY datemodified DESC;");
+	//Store the database results in an array
+	$searchresults = array();
+	while ($row = mysqli_fetch_array($result)){
+		$searchresults[] = $row;	//Array of doc arrays
+	}
+	$table = new doctable;
+	$table->maketable($searchresults);
+}
+
+//If a specific document has been requested:
+if(isset($_GET['doc'])){
+	$docid = $_GET['doc'];
+	$result = mysqli_query($mysqli,"SELECT * FROM docnums WHERE number = '".$docid."';");
+	//Store the database results in an array
+	
+	$doc = mysqli_fetch_array($result);
+	echo "Number: ".$doc['number']." Description: ".$doc['description']."<br>";
+	echo "
+		<form>
+		<table>
+			<tr>
+				<td>Document Number:</td><td>
+			</tr>
+	";
 }
 
 
